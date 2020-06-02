8773aa38b52497fd5438a8339932929d056df3c5
XIAOLONG LI <1491skdpf89@gmail.com>
2020-06-02 18:01:14 -0500

    PTAL!
    1. added comments
    2. deleted ' string = Word' , why do you need this line
    3. 
              for x in range(n):
                   return string[:3] * n
    This won't work if n = 0.

--- warm-2/front_times.py
+++ warm-2/front_times.py
@@ -1,11 +1,12 @@
 def front_times(str, n):
+  # method 1:
   return str[:3] * n
-  
+  # method 2:
   if len(str) >= 3:
     return str[:3] * n
   else:
     return str * n
-    
+  #method 3: 
   string = Word
   for x in range(n):
     return string[:3] * n
