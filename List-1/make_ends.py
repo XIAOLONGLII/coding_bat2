fd314ae8b5f353a83544e9a685e0a796d4010828
XIAOLONG LI <1491skdpf89@gmail.com>
2020-06-09 17:10:29 -0400

    Added close comments """
    See title. 
    PTAL

--- List-1/make_ends.py
+++ List-1/make_ends.py
@@ -10,7 +10,7 @@ Given an array of ints, return a new array length 2 containing the first and las
 make_ends([1, 2, 3]) → [1, 3]
 make_ends([1, 2, 3, 4]) → [1, 4]
 make_ends([7, 4, 6, 2]) → [7, 2]
-
+"""
 
 def make_ends(nums):
   return [nums[0], nums[-1]]
